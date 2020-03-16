2e1dc88bb3650882709927c68b6a1a1aac73c74a
Chris Wong <wc38.wong@gmail.com>
2020-03-16 10:12:06 +0800

    Test to check package.json existence

--- test/cmd.test.js
+++ test/cmd.test.js
@@ -1,14 +1,34 @@
-require('chai').should()
+const { execSync, spawn } = require('child_process')
+const { Buffer } = require('buffer')
+const { expect } = require('chai')
 const tmp = require('tmp')
-const { log } = require('../lib/util')
+const { error } = require('../lib/util')
 
 describe('command line', () => {
-	it('.git folder exists', () => {
-		tmp.file(function _tmpFileCreated(err, path, fd) {
-			if(err) throw err
+	const messages = ['package.json exists.']
+	let containMessage = new Array(messages.length).fill(false)
 
-			log('File: ', path)
-			log('Filedescriptor: ', fd)
+	it(messages[0], () => {
+		const tmpDir = tmp.dirSync()
+		const options = {
+			cwd: tmpDir.name
+		}
+		const node_init_util = spawn('node-init-util', [], options)
+		execSync('echo "{}" >> package.json', options)
+		node_init_util.stdout.on('data', (data) => {
+			const result = Buffer.from(data).toString('ascii')
+			if(result.includes(messages[0])) {
+				containMessage[0] = true
+			}
+		})
+
+		node_init_util.stderr.on('data', (data) => {
+			error('stderr:')
+			error(Buffer.from(data).toString('ascii'))
+		})
+
+		node_init_util.on('close', () => {
+			expect(containMessage[0]).to.be.true
 		})
 	})
 })
